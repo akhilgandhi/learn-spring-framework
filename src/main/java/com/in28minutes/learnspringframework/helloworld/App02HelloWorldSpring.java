df9cc457a0e3173764a38ebfc2377d48221f2f9e
Akhil Gandhi <akhilgandhi@live.in>
2023-01-31 18:41:21 +0530

    Closing the App Context

--- src/main/java/com/in28minutes/learnspringframework/helloworld/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/helloworld/App02HelloWorldSpring.java
@@ -8,12 +8,13 @@ public class App02HelloWorldSpring {
 
     public static void main(String[] args) {
         // Launch a spring application context - AnnotationConfigApplicationContext class
-        var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-        // Configure the things that we want Spring to manage - @Configuration class
-        System.out.println(context.getBean("name") + " " + context.getBean("age"));
-        System.out.println(context.getBean("person") + " " + context.getBean("address2"));
-        System.out.println(context.getBean(Address.class));
-        System.out.println("Method Call " + context.getBean("person2MethodCall"));
-        System.out.println("Parameters " + context.getBean("person3Parameters"));
+        try (var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class)) {
+            // Configure the things that we want Spring to manage - @Configuration class
+            System.out.println(context.getBean("name") + " " + context.getBean("age"));
+            System.out.println(context.getBean("person") + " " + context.getBean("address2"));
+            System.out.println(context.getBean(Address.class));
+            System.out.println("Method Call " + context.getBean("person2MethodCall"));
+            System.out.println("Parameters " + context.getBean("person3Parameters"));
+        }
     }
 }

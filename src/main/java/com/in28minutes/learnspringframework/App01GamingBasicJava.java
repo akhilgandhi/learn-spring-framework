32fd98192bfc095e847182db8f4e93ea9e9a493f
Akhil Gandhi <akhilgandhi@live.in>
2023-02-02 14:11:49 +0530

    Iteration 3

--- src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
@@ -1,7 +1,8 @@
 package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
-import com.in28minutes.learnspringframework.game.PacMan;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App01GamingBasicJava {
 
@@ -14,12 +15,18 @@ public class App01GamingBasicJava {
          * Iteration 2 - Add an interface GamingConsole to disconnect GameRunner
          *               class from Game(Mario, SuperContra).Now Game(Mario, SuperContra)
          *               will implement the GamingConsole interface.
+         * Iteration 3 - Using Spring beans in the Gaming Application
          */
 //        var game = new MarioGame();
 //        var game = new SuperContraGame();
-        var game = new PacMan();                // 1: Object creation
-        var gameRunner = new GameRunner(game);  // 2: Object creation + wiring dependencies
-                                                // Game is a dependency of GameRunner
-        gameRunner.run();
+//        var game = new PacMan();                // 1: Object creation
+//        var gameRunner = new GameRunner(game);  // 2: Object creation + wiring dependencies
+//                                                // Game is a dependency of GameRunner
+//        gameRunner.run();
+
+        try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+            context.getBean(GamingConsole.class).up();
+            context.getBean(GameRunner.class).run();
+        }
     }
 }
